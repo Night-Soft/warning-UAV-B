7ab585f6b788a84869a6f2a23f951538298e74db
Night-Soft <yurafisher2@gmail.com>
2025-12-14 23:06:33 +0200

    added TG_TOKEN

--- src/config.js
+++ src/config.js
@@ -15,3 +15,5 @@ export const { CHANELS, POPULATED_AREAS } = JSON.parse(process.env.KEYWORDS);
 export const D_AREA = process.env.D_AREA;
 export const D_AREA_EXCLUDES = JSON.parse(process.env.D_AREA_EXCLUDES).excludes;
 export const SUMYGO_KEYS = JSON.parse(process.env.SUMYGO_KEYS).keys;
+export const TG_TOKEN = process.env.TG_TOKEN;
+
